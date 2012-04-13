86e17c2dfa6d4d6f2246fd9cfdcaff186b977670
Lion Vollnhals <lion.vollnhals@googlemail.com>
2012-04-13 22:48:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FayeRainbows::Application.config.secret_token = '3d5ad55ee43a184825ac6d194d72d65943f574f8bc09fe0ed0cfe36b7579f0143d32189d25b7ac50a4f4ac568f8d01194c6139c098ca94dabbdf1dfe2c3dc85c'
